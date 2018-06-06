1896947eaae0dfda8453d74a5843584370df47bc
CHERRYGAMES\DK_RZ <optimusdrift@gmail.com>
2018-06-06 16:36:52 -0300

    comitazo

--- Punto1/php/Punto1.php
+++ Punto1/php/Punto1.php
@@ -2,7 +2,9 @@
     public function FunctionName(Type $var = null)
     {
         for ($i=0; $i < ; $i++) { 
-            
+            for ($i=0; $i < ; $i++) { 
+                # code...
+            }
         }
     }
 ?>
